2390a6b10c612946f9999ff485d5dde0788c182c
Roberto James <roberto.junior.james13@gmail.com>
2025-07-29 13:27:11 -0500

    added spring boot files and moved html into src/main/resources/templates/

---
+++ src/test/java/com/roberto/studentmanagement/Student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.studentmanagement.Student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
